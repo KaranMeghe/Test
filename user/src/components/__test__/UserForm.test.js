178caa2bc91ea633cc8c1352f774d097dc04ab32
Karan Meghe <karanmeghe87@gmail.com>
2024-01-03 18:24:33 +0530

    Refactor Test - Querying elements by Labels

--- user/src/components/__test__/UserForm.test.js
+++ user/src/components/__test__/UserForm.test.js
@@ -19,15 +19,24 @@ test("it shows two inputs and a button", () => {
 
 test("it calls onUserAdd when the form is submitted", async () => {
   // NOT THE BEST IMPLEMENTATION
-  const argList = [];
-  const callback = (...args) => {
-    argList.push(args);
-  };
+  //   const argList = [];
+  //   const callback = (...args) => {
+  //     argList.push(args);
+  //   };
+
+  // Mock
+  const mock = jest.fn();
   // Try to render my component
-  render(<UserForm onUserAdd={callback} />);
+  render(<UserForm onUserAdd={mock} />);
 
   // Find the two inputs
-  const [nameInput, emailInput] = screen.getAllByRole("textbox");
+  const nameInput = screen.getByRole("textbox", {
+    name: /name/i,
+  });
+
+  const emailInput = screen.getByRole("textbox", {
+    name: /email/i,
+  });
 
   // Simulate typing in a name
   await user.click(nameInput);
@@ -44,6 +53,6 @@ test("it calls onUserAdd when the form is submitted", async () => {
   await user.click(button);
 
   // Assertion to make sure 'onUserAdd' gets called with email/name
-  expect(argList).toHaveLength(1);
-  expect(argList[0][0]).toEqual({ name: "jane", email: "jane@jane.com" });
+  expect(mock).toHaveBeenCalled();
+  expect(mock).toHaveBeenCalledWith({ name: "jane", email: "jane@jane.com" });
 });
